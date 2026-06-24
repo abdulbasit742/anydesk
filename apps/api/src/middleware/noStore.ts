ed2cac9f7538a79df32618b63ef6f358d15820cd
ABDUL BASIT <absh5506@gmail.com>
2026-06-24 11:11:57 +0500

    Add no-store response middleware

---
+++ apps/api/src/middleware/noStore.ts
@@ -0,0 +1,8 @@
+import type { RequestHandler } from "express";
+
+export const noStore: RequestHandler = (_req, res, next) => {
+  res.setHeader("Cache-Control", "no-store");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  next();
+};
