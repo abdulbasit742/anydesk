1c60ffc6295478f52a33ff4333cfb22866648cab
ABDUL BASIT <absh5506@gmail.com>
2026-06-25 11:31:21 +0500

    Export clipboard sync readiness summary helpers

--- packages/shared/src/clipboard/index.ts
+++ packages/shared/src/clipboard/index.ts
@@ -1,4 +1,5 @@
 export * from "./clipboardReadiness.js";
+export * from "./clipboardReadinessSummary.js";
 export * from "./constants.js";
 export * from "./helpers.js";
 export * from "./types.js";
